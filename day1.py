29e9c345d971a8086b0199f883f426460fe4087b
santhoshshivakumar11-cpu <santhoshshivakumar11@gmail.com>
2026-04-20 14:59:29 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("Hello MT Mulan - Wiper starts IT journey")
+name = "Santhosh"
+rank = "Wiper" 
+print(f"{name} is {rank} today, Developer tomorrow")
+print("Day 1: Variables and Print complete")
