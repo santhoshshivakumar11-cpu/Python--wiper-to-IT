2a45431bba19d605f5ea0d55b9b7650d7414f715
santhoshshivakumar11-cpu <santhoshshivakumar11@gmail.com>
2026-04-21 15:06:34 +0530

    Create day3.py
    Day3:loops+len()mastered

---
+++ day3.py
@@ -0,0 +1,14 @@
+# Day 3: Wiper learning loops on ship
+
+print("Wiper → IT Progress:")
+
+tasks = ["Wake 07:00", "12-12 Shift", "Code 21:00", "No Zero Days"]
+
+for task in tasks:
+    print(f"✅ {task}")
+
+skills = ["Python", "GitHub", "Loops", "No Zero Days"]
+print(f"Skills unlocked: {len(skills)}")
+
+print("Today = Day 3 SHIPPED")
+
