b1bf5748bf168526784917438e6bca050da688ca
santhoshshivakumar11-cpu <santhoshshivakumar11@gmail.com>
2026-04-22 15:01:21 +0530

    Day4-if/else decision code

---
+++ day4.py
@@ -0,0 +1,11 @@
+# Day 4: Code decides like Chief
+duty = "off"  # Change this to "on" and test
+
+if duty == "off":
+    print("Status: OFF DUTY → Time to code")
+    print("Task: Learning if/else for IT job")
+else:
+    print("Status: ON WATCH → Focus on ship safety")
+    print("Code later: No zero days rule")
+
+print("Day 4 = Decision making unlocked")
